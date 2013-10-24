3b23325217c9ab9c09d57fc78ed63267223872d0
unknown <Pramook Khungurn@yozakura.cs.cornell.edu>
2013-10-24 19:54:00 -0400

    more work on extracting info from the graph

--- crawl.rb
+++ crawl.rb
@@ -13,64 +13,70 @@ ActiveRecord::Base.establish_connection("development")
 #	puts "Cannot find unexplored item!"	
 #end
 
-while true	
-	item = Item.where(:content_tree_explored => false).first	
-	if !item.nil?
-		puts "Considering item with Nico ID: #{item.nicoid} ... "		
-		if item.title.nil?
-			puts "Item has no title, so fetching title info ..."
-			item.fetch_info
+while true
+	begin
+		while true	
+			item = Item.where(:content_tree_explored => false).first	
+			if !item.nil?
+				puts "Considering item with Nico ID: #{item.nicoid} ... "		
+				if item.title.nil?
+					puts "Item has no title, so fetching title info ..."
+					item.fetch_info
+				end
+
+				if item.is_mmd			
+					item.fetch_content_tree_info
+				else
+					puts "Item is not an MMD item. Skipping content tree lookup... "
+					item.content_tree_explored = true
+					item.save
+				end
+			end
+			found_content_tree = !item.nil?
+
+			item_count = Item.count
+			explored_count = Item.where(:content_tree_explored => true).count
+			puts
+			puts "Content tree explored: #{explored_count} out of #{item_count}"
+			link_count = Reference.where(:kind => "ct").count
+			puts "There are currently #{link_count} content tree links."
+			puts
+			
+			sleep(1)
+
+			item = Item.where(:description_explored => false).first			
+			if !item.nil?		
+				puts "Considering item with Nico ID: #{item.nicoid} ... "
+				if item.title.nil?
+					puts "Item has no title, so fetching title info ..."
+					item.fetch_info
+				end
+
+				if item.is_mmd			
+					item.fetch_description_info
+				else
+					puts "Item is not an MMD item. Skipping description lookup ... "
+					item.description_explored = true
+					item.save
+				end
+			end
+			found_text = !item.nil?
+
+			item_count = Item.count
+			explored_count = Item.where(:description_explored => true).count
+			puts
+			puts "Description explored: #{explored_count} out of #{item_count}"
+			link_count = Reference.where(:kind => "text").count
+			puts "There are currently #{link_count} text links."
+			puts
+
+			sleep(1)
+
+			if !found_content_tree && !found_text
+				exit
+			end
 		end
-
-		if item.is_mmd			
-			item.fetch_content_tree_info
-		else
-			puts "Item is not an MMD item. Skipping content tree lookup... "
-			item.content_tree_explored = true
-			item.save
-		end
-	end
-	found_content_tree = !item.nil?
-
-	item_count = Item.count
-	explored_count = Item.where(:content_tree_explored => true).count
-	puts
-	puts "Content tree explored: #{explored_count} out of #{item_count}"
-	link_count = Reference.where(:kind => "ct").count
-	puts "There are currently #{link_count} content tree links."
-	puts
-	
-	sleep(1)
-
-	item = Item.where(:description_explored => false).first			
-	if !item.nil?		
-		puts "Considering item with Nico ID: #{item.nicoid} ... "
-		if item.title.nil?
-			puts "Item has no title, so fetching title info ..."
-			item.fetch_info
-		end
-
-		if item.is_mmd			
-			item.fetch_description_info
-		else
-			puts "Item is not an MMD item. Skipping description lookup ... "
-			item.description_explored = true
-			item.save
-		end
-	end
-	found_text = !item.nil?
-
-	item_count = Item.count
-	explored_count = Item.where(:description_explored => true).count
-	puts
-	puts "Description explored: #{explored_count} out of #{item_count}"
-	link_count = Reference.where(:kind => "text").count
-	puts "There are currently #{link_count} text links."
-	puts
-
-	sleep(1)
-
-	if !found_content_tree && !found_text
-		break
+	rescue 
+		# NOP
 	end
 end
